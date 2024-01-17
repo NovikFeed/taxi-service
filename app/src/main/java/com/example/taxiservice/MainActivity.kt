a6dd522170c1306b1e244af91a29a72f14537d53
NovikFeed <vania200345@gmail.com>
2024-01-17 23:16:59 +0300

    Fix bugs

--- app/src/main/java/com/example/taxiservice/MainActivity.kt
+++ app/src/main/java/com/example/taxiservice/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.taxiservice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent = Intent(this, LoginActivity::class.java)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(intent)
+            finish()
+        },5000)
     }
 }
