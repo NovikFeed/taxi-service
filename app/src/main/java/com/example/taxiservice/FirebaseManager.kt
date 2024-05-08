e75af0bc26266b063b798711937463b61d0842c5
ivanovich <vania200345@gmail.com>
2024-05-08 11:40:24 +0300

    Add manager for get user and order from server

--- app/src/main/java/com/example/taxiservice/FirebaseManager.kt
+++ app/src/main/java/com/example/taxiservice/FirebaseManager.kt
@@ -7,9 +7,9 @@ import com.google.firebase.database.getValue
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-class OrderManager {
+class FirebaseManager {
     private val databaseRef = Firebase.database("https://taxiservice-ef804-default-rtdb.europe-west1.firebasedatabase.app/").reference
-    private fun getOrderInClass(uid : String, callback : (Order) -> Unit){
+     fun getOrder(uid : String, callback : (Order) -> Unit){
         val dbOrder = databaseRef.child("order").child(uid)
         dbOrder.addValueEventListener(object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
@@ -24,4 +24,21 @@ class OrderManager {
 
         })
     }
+     fun getUser(uidUser: String, callback: (User) -> Unit){
+         val dbUsers = databaseRef.child("users").child(uidUser)
+         dbUsers.addValueEventListener(object : ValueEventListener{
+             override fun onDataChange(snapshot: DataSnapshot) {
+                 if(snapshot.exists()){
+                     val dataUser = snapshot.getValue<User>()
+                     dataUser?.let{
+                         callback(it)
+                     }
+                 }
+             }
+
+             override fun onCancelled(error: DatabaseError) {
+             }
+
+         })
+     }
 }
